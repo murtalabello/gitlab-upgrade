c3c1507d6d3801e5cf6207c03deeefcd61353256
murtalabello <61387158+murtalabello@users.noreply.github.com>
2024-12-13 09:56:05 -0600

    Update version_check.sh

--- version_check.sh
+++ version_check.sh
@@ -1,30 +1,23 @@
 #!/bin/bash
 
 # Retrieve current GitLab version
-current_version=$(sudo /opt/gitlab/bin/gitlab-rake gitlab:env:info | grep -i "Version:" | awk '{print $2}' | head -1)
+current_version=$(sudo /opt/gitlab/bin/gitlab-rake gitlab:env:info | grep -i "Version:" | awk 'NR==1{print $2}')
 
 # Target version passed as an argument
 target_version="$1"
 
+# Log current and target versions for debugging
+echo "Current GitLab version: $current_version"
+echo "Target GitLab version: $target_version"
+
 # Define valid upgrade paths
-valid_upgrade_paths=(
-  "16.10.10-ee:17.0"
-  "17.0:17.1"
-  "17.1:17.3"
-)
+declare -A valid_upgrade_paths
+valid_upgrade_paths["16.10.10-ee"]="17.0"
+valid_upgrade_paths["17.0"]="17.1"
+valid_upgrade_paths["17.1"]="17.3"
 
-# Function to validate upgrade path
-validate_upgrade_path() {
-  for path in "${valid_upgrade_paths[@]}"; do
-    if [[ "$current_version:$target_version" == "$path" ]]; then
-      return 0
-    fi
-  done
-  return 1
-}
-
-# Check if upgrade is valid
-if validate_upgrade_path; then
+# Validate upgrade path
+if [[ "${valid_upgrade_paths[$current_version]}" == "$target_version" ]]; then
   echo "Direct upgrade from $current_version to $target_version is possible."
   exit 0
 else
