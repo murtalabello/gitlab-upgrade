11f20732f503145542e52a64c5c99688b0c4fbbc
murtalabello <61387158+murtalabello@users.noreply.github.com>
2024-12-13 09:48:22 -0600

    Update version_check.sh

--- version_check.sh
+++ version_check.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
 
+# Retrieve current GitLab version
 current_version=$(sudo /opt/gitlab/bin/gitlab-rake gitlab:env:info | grep "Version:" | awk '{print $2}')
 
+# Target version
 target_version="17.3"
 
-case "$current_version" in
-  "16.10.10-ee")
-    if [[ "$target_version" == "17.0" ]]; then
-      echo "Direct upgrade from $current_version to $target_version is possible."
-      exit 0
-    fi
-    ;;
-  "17.0")
-    if [[ "$target_version" == "17.3" ]]; then
-      echo "Direct upgrade from $current_version to $target_version is possible."
-      exit 0
-    fi
-    ;;
-  *)
-    echo "Direct upgrade from $current_version to $target_version is NOT possible."
-    echo "Please upgrade to an intermediate version first."
-    exit 1
-    ;;
-esac
+if [[ "$current_version" == "16.10.10-ee" && "$target_version" == "17.0" ]]; then
+  echo "Direct upgrade from $current_version to $target_version is possible."
+  exit 0
+elif [[ "$current_version" == "17.0" && "$target_version" == "17.3" ]]; then
+  echo "Direct upgrade from $current_version to $target_version is possible."
+  exit 0
+else
+  echo "Direct upgrade from $current_version to $target_version is NOT possible. Upgrade to intermediate version first."
+  exit 1
+fi
